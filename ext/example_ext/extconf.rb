5b63ba60d795eb9fee283bdb2dc9735c43374996
Wataru Ashihara <wataash@wataash.com>
2021-08-14 16:17:19 +0900

    -ggdb3 -O0

--- ext/example_ext/extconf.rb
+++ ext/example_ext/extconf.rb
@@ -1,3 +1,13 @@
+require "rbconfig"
+if RbConfig::MAKEFILE_CONFIG["CFLAGS"].include?("-g -O2")
+  fixed_CFLAGS = RbConfig::MAKEFILE_CONFIG["CFLAGS"].sub("-g -O2", "$(cflags)")
+  puts("Fix CFLAGS: #{RbConfig::MAKEFILE_CONFIG["CFLAGS"]} -> #{fixed_CFLAGS}")
+  RbConfig::MAKEFILE_CONFIG["CFLAGS"] = fixed_CFLAGS
+end
+
 require "mkmf"
+
+CONFIG["optflags"] = "-O0"
+CONFIG["debugflags"] = "-ggdb3"
 
 create_makefile("example_ext/example_ext")
